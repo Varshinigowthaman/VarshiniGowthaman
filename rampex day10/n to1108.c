19f3f18868fac807bf45baea95196bc33bacadcf
Varshinigowthaman <dhanuvarshu2002@gmail.com>
2025-05-23 09:43:07 +0530

    Add files via upload

--- rampex day10/n to1108.c
+++ rampex day10/n to1108.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
-void print_n_to_1(int n) {
+void print_n_to_1(int n)
+{
   if (n >= 1) {
     printf("%d ", n);
     print_n_to_1(n - 1);
   }
 }
-
 int main() {
   int num;
   printf("Enter a positive integer: ");
   scanf("%d", &num);
-
   printf("Numbers from %d to 1: ", num);
   print_n_to_1(num);
   printf("\n");
-
   return 0;
 }
